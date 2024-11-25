b7597f86287621e9702dfd49f7bed834c9e2d15a
4463fger <3204033779@qq.com>
2024-11-25 21:40:35 +0800

    LeetCode162.寻找峰值

---
+++ Learn/06_二分查找/Code04_FindPeakElement.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: LeetCode162寻找峰值
+ * │  类    名: FindPeakElement.cs
+ * │  创 建 人: 4463fger
+ * └──────────────────────────────────┘
+ */
+
+namespace LeetCode;
+// 题目:https://leetcode.cn/problems/find-peak-element/description/
+class Solution
+{
+    public static int FindPeakElement(int[] arr)
+    {
+        int n = arr.Length;
+        if (arr.Length == 1)
+        {
+            return 0;
+        }
+
+        if (arr[0] > arr[1])
+        {
+            return 0;
+        }
+
+        if (arr[n - 1] > arr[n - 2])
+        {
+            return n - 1;
+        }
+
+        int l = 1, r = n - 2, m = 0, ans = -1;
+        while (l <= r)
+        {
+            m = (l + r) >> 1;
+            if (arr[m - 1] > arr[m])
+            {
+                r = m - 1;
+            }
+            else if (arr[m] < arr[m + 1])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                ans = m;
+                break;
+            }
+        }
+        return ans;
+    }
+}
