3f33c563b95852cfda5f3de1be9b3499dc30e6ca
4463fger <3204033779@qq.com>
2024-11-25 21:06:54 +0800

    二分查找

---
+++ Learn/06_二分查找/Code02_FindLeft.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: 寻找>=num的最左位置
+ * │  类    名: Code02_FindLeft.cs
+ * │  创 建 人: 4463fger
+ * └──────────────────────────────────┘
+ */
+namespace Learn;
+
+// 有序数组中寻找>=num的最左位置
+public class Code02_FindLeft
+{
+    public static int findLeft(int[] arr, int num)
+    {
+        int l = 0, r = arr.Length - 1, m = 0;
+        int ans = -1;
+        while (l <= r)
+        {
+            m = l + ((r - l) >> 1);
+            if (arr[m] >= num)
+            {
+                // 如果中间位置的值大于等于num，更新答案并继续在左半边搜索
+                ans = m;
+                r = m - 1;
+            }
+            else
+            {
+                // 否则在右半边搜索
+                l = m + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int right(int[] arr, int num)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] >= num)
+                return i;
+        }
+
+        return -1;
+    }
+}
---
+++ Learn/06_二分查找/Code03_FindRight.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: 寻找>=num的最右位置
+ * │  类    名: Code02_FindLeft.cs
+ * │  创 建 人: 4463fger
+ * └──────────────────────────────────┘
+ */
+
+namespace Learn;
+
+class Code03_FindRight
+{
+    // 保证arr有序，才能用这个方法
+    // 有序数组中找<=num的最右位置
+    static int FindRight(int[] arr, int num)
+    {
+        int l = 0, r = arr.Length - 1, m = 0;
+        int ans = -1;
+        while (l <= r)
+        {
+            m = l + ((r - l) >> 1);
+            if (arr[m] <= num)
+            {
+                ans = m;
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // 保证arr有序，才能用这个方法
+    static int Right(int[] arr, int num)
+    {
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            if (arr[i] <= num)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
