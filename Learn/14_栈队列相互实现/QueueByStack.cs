3417b85054495ecf20765e7dd9a18e2468ac8afe
4463fger <3204033779@qq.com>
2024-11-30 09:32:05 +0800

    LeetCode232. 用栈实现队列

---
+++ Learn/14_栈队列相互实现/QueueByStack.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: LeetCode232. 用栈实现队列
+ * │  类    名: QueueByStack.cs
+ * │  创 建 人:  4463fger
+ * └──────────────────────────────────┘
+ */
+
+// 用栈实现队列
+class QueueByStack
+{
+    public class MyQueue 
+    {
+        Stack<int> inStack = new();
+        Stack<int> outStack = new();
+        public MyQueue() {   
+        }
+    
+        // 倒数据
+        // 从in栈，把数据倒入out栈
+        // 1) out空了，才能倒数据
+        // 2) 如果倒数据，in必须倒完
+        private void InToOut()
+        {
+            if(outStack.Count == 0)
+                while(inStack.Count != 0)
+                    outStack.Push(inStack.Pop());
+        }
+
+        public void Push(int x) 
+        {
+            inStack.Push(x);
+            InToOut();
+        }
+    
+        public int Pop() {
+            InToOut();
+            return outStack.Pop();
+        }
+    
+        public int Peek() {
+            InToOut();
+            return outStack.Peek();
+        }
+    
+        public bool Empty() {
+            return inStack.Count== 0 && outStack.Count== 0;
+        }
+    }
+}
