330add8411d872d2056713e87b53d4ed971a765d
4463fger <3204033779@qq.com>
2024-12-15 10:36:35 +0800

    83.删除排序链表中的重复元素

--- Practice/1.链表/1.Easy/83.DeleteDuplicates.cs
+++ Practice/1.链表/1.Easy/83.DeleteDuplicates.cs
@@ -6,34 +6,38 @@
  * └──────────────────────────────────┘
  */
 
-public class DeleteDuplicatesSolution
+namespace LeetCode
 {
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
+ public class DeleteDuplicatesSolution
+ {
+  public class ListNode
+  {
+   public int val;
+   public ListNode next;
 
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
+   public ListNode(int val = 0, ListNode next = null)
+   {
+    this.val = val;
+    this.next = next;
+   }
+  }
 
-    public ListNode DeleteDuplicates(ListNode head)
+  public ListNode DeleteDuplicates(ListNode head)
+  {
+   ListNode cur = head;
+   while (cur != null && cur.next != null)
+   {
+    if (cur.val == cur.next.val)
     {
-        ListNode cur = head;
-        while (cur != null && cur.next != null)
-        {
-            if (cur.val == cur.next.val)
-            {
-                cur.next = cur.next.next;
-            }
-            else
-            {
-                cur = cur.next;
-            }
-        }
-        return head;
+     cur.next = cur.next.next;
     }
+    else
+    {
+     cur = cur.next;
+    }
+   }
+   return head;
+  }
+ }
+
 }
