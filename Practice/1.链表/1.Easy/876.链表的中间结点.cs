0e4450f510281fcdf6176e84acd5cab0bd1bf143
4463fger <3204033779@qq.com>
2024-12-18 11:01:35 +0800

    LeetCode876.链表的中间结点

---
+++ Practice/1.链表/1.Easy/876.链表的中间结点.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: LeetCode.876.middle-of-the-linked-list
+ * │  类    名: MiddleNode.cs
+ * │  创 建 人:
+ * └──────────────────────────────────┘
+ */
+
+namespace LeetCode;
+
+
+public class MiddleNodeSolution 
+{
+    public ListNode MiddleNode(ListNode head) 
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
