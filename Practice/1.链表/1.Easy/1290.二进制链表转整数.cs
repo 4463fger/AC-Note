5834462fd92f13d636c216cc16fe6090df8c0b2a
4463fger <3204033779@qq.com>
2024-12-23 15:49:41 +0800

    LeetCode1290.二进制链表转整数

---
+++ Practice/1.链表/1.Easy/1290.二进制链表转整数.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: LeetCode1290.二进制链表转整数
+ * │  类    名: GetDecimalValue.cs
+ * │  创 建 人: 4463fger
+ * └──────────────────────────────────┘
+ */
+
+namespace LeetCode;
+
+public class GetDecimalValueSolution
+{
+    // (result << 1) 等价于 result * 2
+    // (result << 1) | head.val 等价于 result * 2 + head.val
+    public int GetDecimalValue(ListNode head)
+    {
+        var result = 0;
+        while (head != null)
+        {
+            result = (result << 1) | head.val;
+            head = head.next;
+        }
+        return result;
+    }
+}
