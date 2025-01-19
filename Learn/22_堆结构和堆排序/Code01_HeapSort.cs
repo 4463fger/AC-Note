a578cf294278a6201837a15ffa26775a11d8a2cc
4463fger <3204033779@qq.com>
2025-01-19 12:55:27 +0800

    堆结构和堆排序

---
+++ Learn/22_堆结构和堆排序/Code01_HeapSort.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: 堆结构和堆排序
+ * │  类    名: HeapSort.cs
+ * │  创    建: By 4463fger
+ * └──────────────────────────────────┘
+ */
+ 
+// 堆结构和堆排序，acm练习风格
+// 测试链接 : https://www.luogu.com.cn/problem/P1177
+
+public class Code01_HeapSort
+{
+    private const int MAXN = 100001;
+    private static int[] arr = new int[MAXN];
+    private static int n;
+
+    public static void _Main_(string[] args)
+    {
+        // Luogu();
+        // 从标准输入读取数据
+        using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
+            // 向标准输出写入数据
+        using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput()))
+        {
+            // 读取第一行输入, 包含一个整数n(数组大小)
+            string[] input = reader.ReadLine().Split(' ');
+            n = int.Parse(input[0]);
+            // 读取第二行输入，包含n个整数(数据)
+            input = reader.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+
+            HeapSort2();
+            for (int i = 0; i < n - 1; i++)
+            {
+                writer.Write(arr[i] + " ");
+            }
+
+            writer.WriteLine(arr[n - 1]);
+        }
+    }
+
+    // i位置的数，向上调整大根堆
+    private static void HeapInsert(int i)
+    {
+        while (arr[i] > arr[(i - 1) / 2])
+        {
+            swap(i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    // i位置的数，向下调整大根堆
+    // 当前堆的大小为size
+    private static void Heapify(int i, int size)
+    {
+        int l = i * 2 + 1;
+        while (l < size)
+        {
+            int best = l + 1 < size && arr[l + 1] > arr[l] ? l + 1 : l;
+            best = arr[best] > arr[i] ? best : i;
+            if (best == i)
+                break;
+            swap(best, i);
+            i = best;
+            l = i * 2 + 1;
+        }
+    }
+
+
+    // 从顶到底建立大根堆，O(n * LogN)
+    // 依次弹出堆内最大值并排好序，O(n * LogN)
+    // 整体时间复杂度O(n * LogN)
+    public static void HeapSort1()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            HeapInsert(i);
+        }
+        int size = n;
+        while (size > 1)
+        {
+            swap(0, --size);
+            Heapify(0, size);
+        }
+    }
+
+    // 从底到顶建立大根堆，O(n)
+    // 依次弹出堆内最大值并排好序，O(n * LogN)
+    // 整体时间复杂度O(n * LogN)
+    private static void HeapSort2()
+    {
+        for (int i = n/2 - 1; i >= 0; i--) 
+        {
+            Heapify(i, n);
+        }
+        int size = n;
+        while (size > 1)
+        {
+            swap(0, --size);
+            Heapify(0, size);
+        }
+    }
+    
+    private static void swap(int i, int j)
+    {
+        (arr[i], arr[j]) = (arr[j], arr[i]);
+    }
+}
---
+++ Learn/22_堆结构和堆排序/Code02_HeapSort.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * ┌──────────────────────────────────┐
+ * │  描    述: 堆结构和堆排序
+ * │  类    名: HeapSort.cs
+ * │  创    建: By 4463fger
+ * └──────────────────────────────────┘
+ */
+
+// 堆结构和堆排序
+// 测试链接 : https://leetcode.cn/problems/sort-an-array/
+public class Code02_HeapSort
+{
+    public int[] SortArray(int[] nums)
+    {
+        if (nums.Length > 1)
+        {
+            // heapSort1为从顶到底建堆然后排序
+            // heapSort2为从底到顶建堆然后排序
+            //HeapSort1(nums);
+            HeapSort2(nums);
+        }
+        return nums;
+    }
+    
+    // 从顶到底建立大根堆，O(n * LogN)
+    // 依次弹出堆内最大值并排好序，O(n * LogN)
+    // 整体时间复杂度O(n * LogN)
+    private void HeapSort1(int[] arr)
+    {
+        int n = arr.Length;
+        // 构建大根堆
+        for (int i = 0; i < n; i++)
+        {
+            HeapInsert(arr,i);
+        }
+        // 排序
+        int size = n;
+        while (size > 1)
+        {
+            // 将最大值，移到数组末尾 || size-1
+            Swap(arr, 0, --size);
+            // 0 位置数变了,重新维持大根堆
+            Heapify(arr, 0, size);
+        }
+    }
+    
+    // 从底到顶建立大根堆，O(n)
+    // 依次弹出堆内最大值并排好序，O(n * LogN)
+    // 整体时间复杂度O(n * LogN)
+    private void HeapSort2(int[] arr)
+    {
+        int n = arr.Length;
+        // 从最后一个非叶子节点开始调整
+        for (int i = n/2 - 1; i >= 0; i--)
+        {
+            Heapify(arr,i,n);
+        }
+        int size = n;
+        while (size > 1)
+        {
+            Swap(arr, 0, --size);
+            Heapify(arr, 0, size);
+        }
+    }
+    
+    // i位置的数,向上调整大根堆
+    // arr[i] = x，x是新来的！往上看，直到不比父亲大，或者来到0 位置(顶)
+    private void HeapInsert(int[] arr, int i)
+    {
+        while (arr[i] > arr[(i-1)/2])
+        {
+            Swap(arr, i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+    
+    // 向下调整大根堆
+    // i位置的数,变小了,又想维持大根堆结构
+    // 堆的大小为size
+    private void Heapify(int[] arr, int i, int size)
+    {
+        int l = i * 2 + 1;
+        while (l < size)
+        {
+            // 左孩子=>l || 右孩子=>l+1
+            // ReSharper disable once GrammarMistakeInComment
+            // 找左右孩子中较大的一个
+            // l+1<size检查右孩子是否存在 , 并且右比左大，那么选右
+            int best = l + 1 < size && arr[l + 1] > arr[l] ? l + 1 : l;
+            // 当前结点比右孩子大 break
+            if(arr[best] <= arr[i])
+                break;
+            Swap(arr,best,i);
+            i = best;
+            l = i * 2 + 1;
+        }
+    }
+
+    private void Swap(int[] arr, int i, int j)
+    {
+        (arr[i], arr[j]) = (arr[j], arr[i]);
+    }
+}
