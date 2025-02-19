160f17181ef9d0cd2869000a113a98da5e2a652b
4463fger <3204033779@qq.com>
2025-02-19 22:59:25 +0800

    位图(BitMap)

--- Learn/28_位图/Code01_Bitset.cs
+++ Learn/28_位图/Code01_Bitset.cs
@@ -1,13 +1,59 @@
-﻿using Learn;
+﻿namespace Learn;
 
+// 位图的实现(必须是连续的范围)
+public class Code01_Bitset
+{
+    private int[] set;
+
+    public Code01_Bitset(int n)
+    {
+        // 需要多少个int来存储n位
+        // (n + 31) / 32 向上取整
+        set = new int[(n + 31) / 32];
+    }
+
+    // [num / 32]: 计算出放在第几个数中
+    // num % 32  : 找出在这个数的第几位
+
+    // 添加数字(将对应的位设为1)
+    public void Add(int num)
+    {
+        // | : 有1则为1
+        set[num / 32] |= 1 << (num % 32);
+    }
+
+    // 移除数字(将对应位置设为0)
+    public void Remove(int num)
+    {
+        // & : 同1为1 , 否则为0
+        set[num / 32] &= ~(1 << (num % 32));
+    }
+
+    // 反转数字位(1 => 0 && 0 => 1)
+    public void Reverse(int num)
+    {
+        // ^ : 异或操作，相同为0，不同为1 (无进位相加)
+        set[num / 32] ^= 1 << (num % 32);
+    }
+
+    // 检查数字是否存在
+    public bool Contains(int num)
+    {
+        return ((set[num / 32] >> (num % 32)) & 1) == 1;
+    }
+}
+
+/// <summary>
+/// 对数器验证
+/// </summary>
 class Program
 {
-    public static void Main(string[] args)
+    public static void __Main__(string[] args)
     {
         int n = 1000;
         int testTimes = 10000;
         Console.WriteLine("测试开始");
-        
+
         Code01_Bitset bitset = new Code01_Bitset(n);
         HashSet<int> hashSet = new HashSet<int>();
 
@@ -41,8 +87,9 @@ class Program
                 }
             }
         }
+
         Console.WriteLine("调用阶段结束");
-        
+
         Console.WriteLine("验证阶段开始");
         for (int i = 0; i < n; i++)
         {
---
+++ Learn/28_位图/Code02_DesignBitsetTest.cs
@@ -0,0 +1,133 @@
+﻿using System.Text;
+
+namespace Learn;
+
+// 位图的实现
+// Bitset是一种能以紧凑形式存储位的数据结构
+// Bitset(int n) : 初始化n个位，所有位都是0
+// void fix(int i) : 将下标i的位上的值更新为1
+// void unfix(int i) : 将下标i的位上的值更新为0
+// void flip() : 翻转所有位的值
+// boolean all() : 是否所有位都是1
+// boolean one() : 是否至少有一位是1
+// int count() : 返回所有位中1的数量
+// String toString() : 返回所有位的状态
+public class Code02_DesignBitsetTest
+{
+    // 测试链接 : https://leetcode-cn.com/problems/design-bitset/
+    public class Bitset
+    {
+        private readonly int[] set; // 存储位的数组
+        private readonly int size; // 位图的总大小
+        private int ones; // 1的数量
+        private bool reverse; // 是否翻转位的标志
+        private int zeros; // 0的数量
+
+        public Bitset(int n)
+        {
+            set = new int[(n + 31) / 32]; // 每个int存储32位，计算所需数组长度 
+            size = n;
+            zeros = n; // 初始所有位为0
+            ones = 0;
+            reverse = false; // 初始不翻转
+        }
+
+        // 把i这个数字加入到位图
+        public void Fix(int i)
+        {
+            var index = i / 32;
+            var bit = i % 32;
+            if (!reverse)
+            {
+                // 位图所有位的状态，维持原始含义
+                // 0 : 不存在
+                // 1 : 存在
+                if ((set[index] & (1 << bit)) == 0)
+                {
+                    zeros--;
+                    ones++;
+                    set[index] |= 1 << bit;
+                }
+            }
+            else
+
+            {
+                // 位图所有位的状态，翻转了
+                // 0 : 存在
+                // 1 : 不存在
+                if ((set[index] & (1 << bit)) != 0)
+                {
+                    zeros--;
+                    ones++;
+                    set[index] ^= 1 << bit;
+                }
+            }
+        }
+
+        // 把i这个数字从位图中移除
+        public void Unfix(int i)
+        {
+            var index = i / 32;
+            var bit = i % 32;
+            if (!reverse)
+            {
+                if ((set[index] & (1 << bit)) != 0)
+                {
+                    ones--;
+                    zeros++;
+                    set[index] ^= 1 << bit;
+                }
+            }
+            else
+            {
+                if ((set[index] & (1 << bit)) == 0)
+                {
+                    ones--;
+                    zeros++;
+                    set[index] |= 1 << bit;
+                }
+            }
+        }
+
+        public void Flip()
+        {
+            reverse = !reverse;
+            var tmp = zeros;
+            zeros = ones;
+            ones = tmp;
+        }
+
+        public bool All()
+        {
+            return ones == size;
+        }
+
+        public bool One()
+        {
+            return ones > 0;
+        }
+
+        public int Count()
+        {
+            return ones;
+        }
+        
+        // 转换为二进制字符串表示
+        public string toString()
+        {
+            var builder = new StringBuilder();
+            for (int i = 0, k = 0, number, status; i < size; k++)
+            {
+                number = set[k];
+                for (var j = 0; j < 32 && i < size; j++, i++)
+                {
+                    status = (number >> j) & 1;
+                    status ^= reverse ? 1 : 0;
+                    builder.Append(status);
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
